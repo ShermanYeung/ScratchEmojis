921d38a0f1915f292c812d18ffb49383593c7c0f
ShermanY <69072386+ShermanYeung@users.noreply.github.com>
2021-11-07 20:26:00 +0800

    Add files via upload

---
+++ Scratch Emojis Dv1.0/BetterEmojis.js
@@ -0,0 +1,125 @@
+var AllImages = []
+
+function getFileUrl(file){
+    return localStorage.getItem("ScratchEmojisExtensionId")+file
+}
+
+function BetterEmojisMain(){
+
+    
+    AllImages = document.getElementsByTagName("img")
+
+    for(let i=0;i<AllImages.length;i++){
+
+        //project page
+        if (AllImages[i].classList.contains("emoji")){
+
+            switch (AllImages[i].getAttribute("alt")){
+                case "meow emoji":
+                    AllImages[i].src = getFileUrl("images/cat.svg")
+                    break
+                case "gobo emoji":
+                    AllImages[i].src = getFileUrl("images/gobo.svg")
+                    break
+                case "waffle emoji":
+                    AllImages[i].src = getFileUrl("images/waffle.svg")
+                    break
+                case "taco emoji":
+                    AllImages[i].src = getFileUrl("images/taco.svg")
+                    break
+                case "sushi emoji":
+                    AllImages[i].src = getFileUrl("images/sushi.svg")
+                    break
+                case "pizza emoji":
+                    AllImages[i].src = getFileUrl("images/pizza.svg")
+                    break
+                case "candycorn emoji":
+                    AllImages[i].src = getFileUrl("images/candycorn.svg")
+                    break
+                case "apple emoji":
+                    AllImages[i].src = getFileUrl("images/apple.svg")
+                    break
+                case "cupcake emoji":
+                    AllImages[i].src = getFileUrl("images/cupcake.svg")
+                    break
+                case "broccoli emoji":
+                    AllImages[i].src = getFileUrl("images/broccoli.svg")
+                    break 
+                case "camera emoji":
+                    AllImages[i].src = getFileUrl("images/camera.svg")
+                    break 
+                case "compass emoji":
+                    AllImages[i].src = getFileUrl("images/compass.svg")
+                    break 
+                case "suitcase emoji":
+                    AllImages[i].src = getFileUrl("images/suitcase.svg")
+                    break 
+                case "map emoji":
+                    AllImages[i].src = getFileUrl("images/map.svg")
+                    break 
+                case "10mil emoji":
+                    AllImages[i].src = getFileUrl("images/10mil.svg")
+                    break 
+                case "binoculars emoji":
+                    AllImages[i].src = getFileUrl("images/binoculars.svg")
+                    break 
+            }
+        }
+
+        //profile page
+        switch (AllImages[i].src){
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/meow.png":
+                AllImages[i].src = getFileUrl("images/cat.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/gobo.png":
+                AllImages[i].src = getFileUrl("images/gobo.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/waffle.png":
+                AllImages[i].src = getFileUrl("images/waffle.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/taco.png":
+                AllImages[i].src = getFileUrl("images/taco.svg")
+                break     
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/sushi.png":
+                AllImages[i].src = getFileUrl("images/sushi.svg")
+                break      
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/pizza.png":
+                AllImages[i].src = getFileUrl("images/pizza.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/candycorn.png":
+                AllImages[i].src = getFileUrl("images/candycorn.svg")
+                break 
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/apple.png":
+                AllImages[i].src = getFileUrl("images/apple.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/cupcake.png":
+                AllImages[i].src = getFileUrl("images/cupcake.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/broccoli.png":
+                AllImages[i].src = getFileUrl("images/broccoli.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/camera.png":
+                AllImages[i].src = getFileUrl("images/camera.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/compass.png":
+                AllImages[i].src = getFileUrl("images/compass.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/suitcase.png":
+                AllImages[i].src = getFileUrl("images/suitcase.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/map.png":
+                AllImages[i].src = getFileUrl("images/map.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/binoculars.png":
+                AllImages[i].src = getFileUrl("images/binoculars.svg")
+                break
+            case "https://cdn.scratch.mit.edu/scratchr2/static/__9ecc54ae4d2c87ba18228b48cd4c0697__/images/easter_eggs/10mil.png":
+                AllImages[i].src = getFileUrl("images/10mil.svg")
+                break
+        }
+    }
+}
+
+var loop = setInterval(function(){
+    BetterEmojisMain()
+},0)
---
+++ Scratch Emojis Dv1.0/ServiceAssigner.js
@@ -0,0 +1,86 @@
+function getFileUrl(file){
+    //get URL by extension id.
+    return chrome.extension.getURL(file)
+}
+var Scripts = []
+
+
+
+
+//CUSTOMIZABLE data
+
+var ScriptSources = [
+    getFileUrl("AddEmojis.js")
+]
+
+//scripts that doesn't require anything to be loaded before inserting the script
+var SpecialScriptSources = [getFileUrl("BetterEmojis.js")]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//functions
+
+function ServiceAssignerMain(){
+
+    //vars init
+    var ExtensionUrl = chrome.extension.getURL("")
+    localStorage.setItem("ScratchEmojisExtensionId",ExtensionUrl)
+
+
+    //push the elements into the list
+    for (let i=0; i<ScriptSources.length; i++){
+
+        Scripts.push(document.createElement("script"))
+        Scripts[i].src = ScriptSources[i]
+
+        document.body.appendChild(Scripts[i])
+    }
+
+}
+
+
+function LoadSpecialScripts(){
+
+    //push the elements into the list
+    for (let i=0; i<SpecialScriptSources.length; i++){
+
+        SpecialScript = document.createElement("script")
+        SpecialScript.src = SpecialScriptSources[i]
+
+        document.body.appendChild(SpecialScript)
+    }
+
+}
+
+
+
+
+LoadSpecialScripts()
+//check and wait until the required elements exist
+var checkEverythingExists = setInterval(function(){
+
+    //requirements
+    if ((!(document.getElementsByClassName("compose-limit compose-valid")[0]==undefined))&&
+    (!(document.getElementsByClassName("compose-limit compose-valid")[0].getElementsByTagName("span")[0]==undefined))&&
+    (!(document.getElementsByClassName("flex-row compose-bottom-row")[0]==undefined))){
+
+        ServiceAssignerMain()
+        clearInterval(checkEverythingExists)
+    }
+
+},0)
